f8eea8bf59a93d7708805027cb480ec818b2b0ba
Luca Temes D'Andrea <lucatemex@gmail.com>
2023-04-01 12:39:16 -0300

    Update First Game
    - som adicionado
    - velocidade alterada
    - jogo reinicia quando chega a 5 pontos (teste)

--- main.py
+++ main.py
@@ -14,10 +14,14 @@ y= altura / 2
 x1= randint(10, 630)
 y1= randint(10, 470)
 contador= 0
+posAtualx= 0
+posAtualy= 0
+xx= 20
 fonte= pygame.font.SysFont('inkfree', 30, True, False)
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Jogo sem nome ainda')
 relogio = pygame.time.Clock()
+som1up= pygame.mixer.Sound('smw_1-up.wav')
 
 while True:
         relogio.tick(120) #framerate
@@ -38,26 +42,35 @@ while True:
                 x= largura
         
         if pygame.key.get_pressed()[K_a]:
-                x=x -3
+                x=x -5
         if pygame.key.get_pressed()[K_d]:
-                x=x +3
+                x=x +5
         if pygame.key.get_pressed()[K_w]:
-                y=y -3
+                y=y -5
         if pygame.key.get_pressed()[K_s]:
-                y=y +3
+                y=y +5
 
-        blueCircle= pygame.draw.circle(tela, (0,0,255), (x, y), 20)#parametros(local de exibição=tela , (r,g,b), (posX, posY), radio)
-        ##redRect= pygame.draw.rect(tela, (230,0,0), (x1, y2, 40, 50))
+        blueCircle= pygame.draw.circle(tela, (0,0,255), (x, y), xx) #parametros(local de exibição=tela , (r,g,b), (posX, posY), radio)
+        #redRect= pygame.draw.rect(tela, (230,0,0), (x1, y2, 40, 50))
         whiteCircle= pygame.draw.circle(tela, (0,255,0), (x1, y1), 5)
-        ## yellowLine= pygame.draw.line(tela, (255,255, 0), (0,250), (640, 250), 5)
+        #yellowLine= pygame.draw.line(tela, (255,255, 0), (0,200), (640, 200), 5)
 
         if blueCircle.colliderect(whiteCircle):
                 x1= randint(10, 630)
                 y1= randint(10, 470)
                 contador+= 1
+                som1up.play()
         
-        if contador == 25:
-                pygame.quit()
+        if contador == 5:
+                largura= 640
+                altura= 480
+                x= largura / 2
+                y= altura / 2
+                x1= randint(10, 630)
+                y1= randint(10, 470)
+                contador= 0
+                posAtualx= 0
+                posAtualy= 0
         
         tela.blit(texto_formato, (450, 40))                   
         ## pygame.draw.rect(tela, (230,0,0), (300, 195, 40, 50))
